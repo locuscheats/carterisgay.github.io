a731dce05a2d9eb60a93c997e74b591a2bfbfe39
locuscheats <locusmodzps3@gmail.com>
2022-05-20 19:26:27 -0400

    LOL

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+var _app = function () {
+    this.id = 0;
+    this.videoElement = null;
+    this.audioElement = null;
+    this.musicVolume = 0.12;
+    this.musicFadeIn = 4000;
+    this.skippedIntro = false;
+    this.backgroundToggler = false;
+    this.shouldIgnoreVideo = false;
+    this.effects = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello"];
+    this.brandDescription = [
+        "cutecarter#9014 - 777175846308806676", 
+        "im gay as fuck -carter 2022", 
+        "i love men! -carter 2022", 
+        "♪ Stinky Skusky - All The Single Furries! ♪ ", 
+        "cutecarter#9014 - 777175846308806676", 
+    ];
+
+
+    this.iconChanger = function (urls, delay) {
+        if (!urls)
+            return;
+
+        delay = delay || 2000;
+
+        var counter = 0;
+        
+        setInterval(function () {
+            if(counter < urls.length) {
+                var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+                link.type = 'image/x-icon';
+                link.rel = 'shortcut icon';
+                link.href = urls[counter];
+                document.getElementsByTagName('head')[0].appendChild(link);
+            }
+            else 
+                counter = 0;
+
+           ++counter;
+        }, delay);
+    }
+};
+
+var app = new _app();
